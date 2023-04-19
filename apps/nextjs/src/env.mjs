b21bbc99d06c28606fc4a3228a455f235d7dbf66
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-19 11:41:22 +0200

    env

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -7,6 +7,7 @@ import { z } from "zod";
 const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
+  CLERK_SECRET_KEY: z.string().optional(),
 
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
 });
@@ -28,6 +29,8 @@ const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
     process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 };
