c55d350629dac5477d8bfe3391d6627e87695985
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-20 13:14:04 +0200

    Revert "use props"
    This reverts commit 7c0d7b4aec8e3f3eb1ae26ecf148083021611223.

--- packages/ui/src/calendar.tsx
+++ packages/ui/src/calendar.tsx
@@ -53,12 +53,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => (
-          <ChevronLeft className="h-4 w-4" {...props} />
-        ),
-        IconRight: ({ ...props }) => (
-          <ChevronRight className="h-4 w-4" {...props} />
-        ),
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
