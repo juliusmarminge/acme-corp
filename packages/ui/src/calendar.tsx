15edd907811eaef18489d5567ea07860b6651a7d
Julius Marminge <julius0216@outlook.com>
2023-04-21 16:27:12 +0200

    fix lint

--- packages/ui/src/calendar.tsx
+++ packages/ui/src/calendar.tsx
@@ -53,8 +53,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => (
+          <ChevronLeft className="h-4 w-4" {...props} />
+        ),
+        IconRight: ({ ...props }) => (
+          <ChevronRight className="h-4 w-4" {...props} />
+        ),
       }}
       {...props}
     />
