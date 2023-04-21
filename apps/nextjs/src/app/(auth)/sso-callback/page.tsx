8008cc7d19d597cb8d2c98a5426e1af135c2be7d
Julius Marminge <julius0216@outlook.com>
2023-04-21 19:55:10 +0200

    test props.params

--- apps/nextjs/src/app/(auth)/sso-callback/page.tsx
+++ apps/nextjs/src/app/(auth)/sso-callback/page.tsx
@@ -4,17 +4,19 @@ import { useEffect } from "react";
 import { useClerk } from "@clerk/nextjs/app-beta/client";
 import type { HandleOAuthCallbackParams } from "@clerk/types";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 /**
  * TODO: Check if this can be a server component or api route
  */
-export default function SSOCallback(props: HandleOAuthCallbackParams) {
+export default function SSOCallback(props: {
+  params: HandleOAuthCallbackParams;
+}) {
   console.log("SSO Callback", props);
   const { handleRedirectCallback } = useClerk();
 
   useEffect(() => {
-    void handleRedirectCallback(props).catch((err) =>
+    void handleRedirectCallback(props.params).catch((err) =>
       console.error("SSO handleRedirectCallback error", err),
     );
   }, [props, handleRedirectCallback]);
