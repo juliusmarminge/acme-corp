7468ef8a25376ec308ad8afa7b628d32d017d743
Julius Marminge <julius0216@outlook.com>
2023-04-21 21:26:16 +0200

    fix mdwr

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -10,23 +10,23 @@ const isPublic = (path: string) => {
 };
 
 export default withClerkMiddleware((req: NextRequest) => {
+  const { userId } = getAuth(req);
+
+  // Redirect to home page if user is already signed in
+  if (userId && req.nextUrl.pathname === "/signin") {
+    const homeUrl = new URL("/", req.url);
+    return NextResponse.redirect(homeUrl);
+  }
+
+  // Allow public paths
   if (isPublic(req.nextUrl.pathname)) {
     return NextResponse.next();
   }
 
-  const { userId } = getAuth(req);
+  // Redirect to sign in page if user is not signed in for non-public paths
   if (!userId) {
     const signInUrl = new URL("/signin", req.url);
-    // signInUrl.searchParams.set("redirect_url", req.url);
     return NextResponse.redirect(signInUrl);
-  }
-
-  console.log({ url: req.url, nextUrl: req.nextUrl });
-
-  if (req.nextUrl.pathname === "/signin") {
-    // Redirect to home page if user is already signed in
-    const homeUrl = new URL("/", req.url);
-    return NextResponse.redirect(homeUrl);
   }
 
   return NextResponse.next();
