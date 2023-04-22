ec810dfd9c589346c5633e2887e1990e0a821625
James Perkins <jamesperkins@hey.com>
2023-04-22 07:59:01 -0400

    Add appearance property to fix user profile sizing

--- apps/nextjs/src/app/(auth)/profile/page.tsx
+++ apps/nextjs/src/app/(auth)/profile/page.tsx
@@ -5,8 +5,16 @@ export const dynamic = "force-dynamic";
 
 export default function ProfilePage() {
   return (
-    <div className="mx-auto flex w-full flex-col justify-center space-y-6 overflow-hidden sm:w-[350px]">
-      <UserProfile />
+    <div className="mx-auto flex w-full max-w-xl flex-col justify-center space-y-6 overflow-hidden">
+      <UserProfile appearance={{
+        elements: {
+          // Main card element
+          card: "w-[500px] w-fit, h-[75vh]",
+          navbar: "hidden",
+          navbarMobileMenuButton: "hidden",
+        }
+      }
+      } />
     </div>
   );
 }
--- apps/nextjs/src/app/@modal/(.)profile/page.tsx
+++ apps/nextjs/src/app/@modal/(.)profile/page.tsx
@@ -8,7 +8,16 @@ export const dynamic = "force-dynamic";
 export default function ProfilePageModal() {
   return (
     <InterceptingModal className="h-[80vh] overflow-hidden">
-      <UserProfile />
+      <UserProfile appearance={{
+        elements: {
+          // Main card element
+          card: "w-[420px] w-fit, h-[75vh]",
+          navbar: "hidden",
+          navbarMobileMenuButton: "hidden",
+        }
+      }
+        // set the routing to virtual, so that the modal can intercept the routing
+      } routing="virtual" />
     </InterceptingModal>
   );
 }
