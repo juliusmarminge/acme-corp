1f6f49ca4c0c76e765eed5ceee3f28d512435e16
juliusmarminge <julius0216@outlook.com>
2023-05-11 14:34:31 +0200

    patch

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,37 +1,20 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const publicPaths = ["/", "/signin*", "/sso-callback*"] as const;
-
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)"))),
-  );
-};
-
-export default withClerkMiddleware((req: NextRequest) => {
-  const { userId } = getAuth(req);
-
-  // Redirect to home page if user is already signed in
-  if (userId && req.nextUrl.pathname === "/signin") {
-    const homeUrl = new URL("/", req.url);
-    return NextResponse.redirect(homeUrl);
-  }
-
-  // Allow public paths
-  if (isPublic(req.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-
-  // Redirect to sign in page if user is not signed in for non-public paths
-  if (!userId) {
-    const signInUrl = new URL("/signin", req.url);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  return NextResponse.next();
+export default authMiddleware({
+  publicRoutes: ["/", "/signin(.*)", "/sso-callback(.*)"],
 });
 
 export const config = {
-  matcher: ["/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - public folder
+     */
+    "/((?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
 };
