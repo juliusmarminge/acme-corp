b49acf425d08884b175b75748e20a624c041e41a
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-05-24 12:11:26 +0100

    fix: duplicate tailwind styles with ui and nextjs app

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: baseConfig.content,
+  content: [
+    ...baseConfig.content,
+    "../../packages/ui/src/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
   presets: [baseConfig],
 } satisfies Config;
