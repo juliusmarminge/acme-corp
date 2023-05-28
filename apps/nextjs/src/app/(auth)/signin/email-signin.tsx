285621f93ee8e6874b230dda7c15074db3b2f598
James Perkins <jamesperkins@hey.com>
2023-05-28 16:14:28 -0400

    Fix the double sends

--- apps/nextjs/src/app/(auth)/signin/email-signin.tsx
+++ apps/nextjs/src/app/(auth)/signin/email-signin.tsx
@@ -4,10 +4,11 @@ import * as React from "react";
 import { useRouter } from "next/navigation";
 import { useSignIn, useSignUp } from "@clerk/nextjs/app-beta/client";
 
-import { Button } from "@acme/ui/button";
-import { Icons } from "@acme/ui/icons";
-import { Input } from "@acme/ui/input";
-import { useToast } from "@acme/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+
 
 export function EmailSignIn() {
   const [isLoading, setIsLoading] = React.useState(false);
@@ -26,9 +27,7 @@ export function EmailSignIn() {
     setIsLoading(true);
     await signIn
       .create({
-        strategy: "email_link",
         identifier: email,
-        redirectUrl: `${window.location.origin}/`,
       })
       .catch((error) => {
         console.log("sign-in error", JSON.stringify(error));
