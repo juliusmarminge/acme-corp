72998506fb5ed0f75141240db9ba29d2e3975c0f
juliusmarminge <julius0216@outlook.com>
2023-05-30 00:57:19 +0200

    use vercel url

--- packages/api/src/env.mjs
+++ packages/api/src/env.mjs
@@ -3,7 +3,10 @@ import * as z from "zod";
 
 export const env = createEnv({
   server: {
-    NEXTJS_URL: z.string().url(),
+    NEXTJS_URL: z.preprocess(
+      (str) => process.env.VERCEL_URL ?? str,
+      process.env.VERCEL ? z.string().min(1) : z.string().url(),
+    ),
 
     STRIPE_API_KEY: z.string(),
     STRIPE_STD_PRODUCT_ID: z.string(),
