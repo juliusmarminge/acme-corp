5f14f82ffed0f6cfbf1fe6b3365c5e0c6122a3b7
juliusmarminge <julius0216@outlook.com>
2023-05-30 15:57:35 +0200

    nit

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -4,9 +4,7 @@ import type { AppRouter } from "./src/root";
 
 export { createTRPCContext, createInnerTRPCContext } from "./src/trpc";
 
-// Export stripe router separately since it is incompatible with Edge runtimes
-// and we want the rest of our endpoints to be running on the Edge => split it up
-export { type AppRouter } from "./src/root";
+export type { AppRouter } from "./src/root";
 
 /**
  * Inference helpers for input types
