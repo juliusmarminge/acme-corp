2ae781efb8dc90b89811aac9eed784297b1cac62
juliusmarminge <julius0216@outlook.com>
2024-01-19 12:45:12 +0100

    chore(deps): bump em'

--- packages/db/prisma/types.ts
+++ packages/db/prisma/types.ts
@@ -2,9 +2,10 @@ import type { ColumnType } from "kysely";
 
 import type { ProjectTier, SubscriptionPlan } from "./enums";
 
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type ApiKey = {
